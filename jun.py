e3fd7236f08731fb9677b1b45c5e3cf034a99764
Cyber-Community01 <101630888+Cyber-Community01@users.noreply.github.com>
2022-03-31 22:14:38 +0700

    Update and rename ju.py to jun.py

--- jun.py
+++ jun.py
@@ -10,6 +10,8 @@ import os
 
 
 
+port = sys.argv[2]
+orgip =ip
 
 Pacotes = [codecs.decode("53414d5090d91d4d611e700a465b00","hex_codec"),#p
                        codecs.decode("53414d509538e1a9611e63","hex_codec"),#c
